6eb49ed5f9c721d1747c89b8be1529563d0d3f24
joann002 <joannrazafinimanana159@gmail.com>
2026-06-22 09:22:30 +0300

    feat: add API resources for JSON responses

---
+++ app/Http/Resources/OrderResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'client' => new ClientResource($this->whenLoaded('client')),
+            'status' => $this->status,
+            'total' => $this->total,
+            'lines' => OrderLineResource::collection($this->whenLoaded('orderLines')),
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
